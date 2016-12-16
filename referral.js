da9db5ca53f88e9e801228b35613e642eb1718e0
hotwing78 <dlwingo@gmail.com>
2016-12-16 04:03:42 -0500

    first

---
+++ referral.js
@@ -0,0 +1,19 @@
+'use strict'
+let referrals = [{
+  fullName: 'Betsy Hare',
+  title: 'Campus Director at The Iron Yard',
+  comment: `Damon is one of the most hardworking people I've ever worked with. He truly cares about his personal growth, the quality of his work,
+  and everyone on his team. He was always looking for ways to improve both his individual work and team. I would jump at the chance
+  to work with him again.`
+},{
+  fullName: 'Alex Castelli',
+  title: 'Application Security Engineer at Qonceptual, Inc',
+  comment:`Damon's passion for development always brings energy to those around him. Even when he has been in class all day at The Iron Yard,
+  he still comes to CODEcamp to learn more! His desire to learn and improve never ceases. Any team would be fortunate to have Damon on board.`
+},{
+  fullName: 'Kaity Miller',
+  title: 'Campus Operations Manager at The Iron Yard',
+  comment:`Damon is an outgoing, driven, knowledge-hungry developer. It is obvious to anyone he encounters that software development is his ultimate calling.
+  He is forever on the search for what he can learn next and how he can be the absolute best at what he does. I have no doubt he will be hugely
+  successful in wherever his passions take him!`
+}];
